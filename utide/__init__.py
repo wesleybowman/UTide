1dc0dd70416eb29c6d49c416a1f3ce4e6d2d5946
Filipe Fernandes <ocefpaf@gmail.com>
2017-09-28 16:10:56 -0300

    make ut_constants and constit_index_dict public

--- utide/__init__.py
+++ utide/__init__.py
@@ -2,9 +2,12 @@ from __future__ import (absolute_import, division, print_function)
 
 from ._solve import solve
 from ._reconstruct import reconstruct
+from ._ut_constants import ut_constants, constit_index_dict
 
 __all__ = ['solve',
-           'reconstruct']
+           'reconstruct',
+           'ut_constants',
+           'constit_index_dict']
 
 from ._version import get_versions
 __version__ = get_versions()['version']
