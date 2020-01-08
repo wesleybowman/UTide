f2a6449405a7fe2818832fded2a2c63b5ff7ec8f
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-01-08 17:03:36 +0000

    Update docs after building Travis build 125 of wesleybowman/UTide
    The docs were built from the branch 'master' against the commit
    c5ac303aef3365c1a93cae7fddfc6a3672a50788.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/wesleybowman/UTide/jobs/634333104.
    
    The doctr command that was run is
    
        /home/travis/miniconda/envs/TEST/lib/python3.7/site-packages/doctr/__main__.py deploy --build-tags --key-path github_deploy_key_wesleybowman_utide.enc --built-docs docs/_build/html dev

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,8 +1,9 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3+8.gd1795e1',
+    VERSION: '0.2.4+2.gc5ac303',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
+    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
