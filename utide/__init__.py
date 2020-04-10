dcf3cc953a696e5bbf3a1d2eac878f0405751e5c
Filipe Fernandes <ocefpaf@gmail.com>
2020-04-10 19:34:11 -0300

    use setuptools_scm and pyproject.toml

--- utide/__init__.py
+++ utide/__init__.py
@@ -1,15 +1,24 @@
+import pkg_resources
+
 from ._solve import solve
 from ._reconstruct import reconstruct
-from ._ut_constants import (ut_constants, constit_index_dict,
-                            hours_per_cycle, cycles_per_hour)
+from ._ut_constants import (
+    ut_constants,
+    constit_index_dict,
+    hours_per_cycle,
+    cycles_per_hour,
+)
 
-__all__ = ['solve',
-           'reconstruct',
-           'ut_constants',
-           'constit_index_dict',
-           'hours_per_cycle',
-           'cycles_per_hour']
+try:
+    __version__ = pkg_resources.get_distribution("utide").version
+except Exception:
+    __version__ = "unknown"
 
-from ._version import get_versions
-__version__ = get_versions()['version']
-del get_versions
+__all__ = [
+    "solve",
+    "reconstruct",
+    "ut_constants",
+    "constit_index_dict",
+    "hours_per_cycle",
+    "cycles_per_hour",
+]
